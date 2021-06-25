ad9724e7a3e95e14cbbb2376e9b18c1db910a085
Brunno Fraga <brunno.fraga@tqi.com.br>
2021-06-25 19:30:08 -0300

    v 1

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
+var fs = require('fs');
 
-const PORT = process.env.PORT || 6000
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+const PORT = process.env.PORT || 3000
+
+app.post('/', function (req, res) {
+    let day = new Date(new Date()).toString().slice(0, 16)
+
+    let log = req.body.log || false
+
+    if (log)
+        fs.appendFile(`./log/${day}.txt`, log + '\n', { encoding: "utf-8", mode: 0o666, flag: "a" }, (err) => {
+            if (err) throw err;
+        });
+
+    res.status(200).json(req.body)
+
 })
 
 app.listen(PORT, () => console.log('LISTENING IN PORT ' + PORT))
