714d55a53bdaebaa95f7efb6d273b96f15e8cc55
Brunno Fraga <brunno.fraga@tqi.com.br>
2021-06-28 12:11:30 -0300

    Pronto

--- index.js
+++ index.js
@@ -7,17 +7,51 @@ app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT || 3000
 
-app.post('/', function (req, res) {
-    let day = new Date(new Date()).toString().slice(0, 16)
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
 
+    if (month.length < 2)
+        month = '0' + month;
+
+    if (day.length < 2)
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+app.post('/:project', function (req, res) {
+
+    let date = formatDate(new Date())
+    let project = req.params.project
     let log = req.body.log || false
 
     if (log)
-        fs.appendFile(`./log/${day}.txt`, log + '\n', { encoding: "utf-8", mode: 0o666, flag: "a" }, (err) => {
+        fs.appendFile(`./log/${project}/${date}.txt`, log + '\n', { encoding: "utf-8", mode: 0o666, flag: "a" }, (err) => {
             if (err) throw err;
         });
 
     res.status(200).json(req.body)
+
+})
+
+
+app.get('/:project/:date', function (req, res) {
+
+    let date = req.params.date || false
+    let project = req.params.project
+
+    if (date) {
+        date = date.split("-").reverse().join("-")
+        try {
+            const file = fs.readFileSync(`./log/${project}/${date}.txt`, 'utf8')
+            res.status(200).send(file)
+        } catch (err) {
+            res.status(400).send()
+        }
+    }
 
 })
 
